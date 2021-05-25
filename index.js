f6be5ee05dc3305876144ade8b34f397e9dcd260
huntergleach <hunter.leach21@gmail.com>
2021-05-25 15:41:01 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+    return string.toUpperCase('Hello!');
+}
+function whisper(string) {
+    return string.toLowerCase('Hello!');
+}
+function logShout(string) {
+    console.log(string.toUpperCase('Hello!'));
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase('Hello!'));
+}
+function sayHiToGrandma(string) {
+    if (string.toLowerCase() === string) {
+        return ("I can't hear you!")
+    } else if (string.toUpperCase() === string) {
+        return ("YES INDEED!")
+    } else if (string === "I love you, Grandma.") {
+        return ("I love you, too.")
+    } 
+}
